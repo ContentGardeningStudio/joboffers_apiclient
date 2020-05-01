fc849214faac7d9821daf052970be1a2dfbaed7b
kamon <kamon.ayeva@gmail.com>
2020-05-01 11:08:29 +0200

    Several improvements and try to make it a bit more reusable

--- joboffers_api_client/main.py
+++ joboffers_api_client/main.py
@@ -1,19 +1,35 @@
 import requests
 
 
-def get_offers(api_url: str, auth_key: str, department_code: str, paginate_max: int):
-    headers = {"Authorization": f"Basic {auth_key}", "Connection": "close"}
+def get_offers(api_url, auth_key="", params=None):
+    """ Get offers by submitting a POST request to an API we use """
 
-    offers = list()
+    headers = {"Connection": "close"}
+    if auth_key:
+        headers["Authorization"] = "Basic {}".format(auth_key)
 
-    for i in range(1, paginate_max + 1):
-        print(f"Get job offers from the results page #{i}")
-        body = {
-            "department_code": department_code,
-            "page": i,
-        }
+    params = params or {}
+
+    # prepare 'body' for the POST request
+    body = {}
+    if "department_code" in params:
+        body["department_code"] = params["department_code"]
+
+    offers = []
+
+    # If there is pagination
+    if "paginate_max" in params:
+        for i in range(1, params["paginate_max"] + 1):
+            print("Getting job offers from the results page #{}...".format(i))
+            body["page"] = i
+            resp = requests.post(api_url, data=body, headers=headers)
+            if resp.status_code == 200:
+                res = list(resp.json())
+                print("==> Found {} job offers from page #{}".format(len(res), i))
+                offers = offers + res
+    else:
+        # case we need to test, with another api example / endpoint
         resp = requests.post(api_url, data=body, headers=headers)
-
-        offers = offers + list(resp.json())
+        offers = list(resp.json())
 
     return offers
